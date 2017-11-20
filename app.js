5cff4c852cf467e2c98423d86145ff4c12a5e17c
MiguelDelacruz <delacruzsmiguel@Gmail.com>
2017-11-20 10:58:39 -1000

    updated

---
+++ app.js
@@ -0,0 +1,31 @@
+var x =5; 
+var y =2;
+var z =4;
+
+
+function calculator(x,y,z) {
+	if (x=="+") {
+		x + y 
+	}
+	else if (x=="-") {
+		x - y
+	}
+	else if (x=="*") {
+		x * z
+	}
+	else if (x=="/") {
+		x / z 
+	}
+	else {
+		output = "assign a operator"
+	}
+
+
+
+
+
+
+}
+
+var output = calculator(x,y,z);
+console.log(output);
