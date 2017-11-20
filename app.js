7c0b910c5d9a3824c9fc1775d3131da34fcfc1c1
MiguelDelacruz <delacruzsmiguel@Gmail.com>
2017-11-20 11:08:33 -1000

    updated

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-var x =5; 
-var y =2;
-var z =4;
+var x = 5;
+
+var y = 2;
+
+var z = 4;
 
 
 function calculator(x,y,z) {
-	if (x=="+") {
-		x + y 
+	if (x=="Add") {
+		x + y;
 	}
-	else if (x=="-") {
-		x - y
+	else if (x=="Subtract") {
+		x - y;
 	}
-	else if (x=="*") {
-		x * z
+	else if (x=="Multiply") {
+		x * z;
 	}
-	else if (x=="/") {
-		x / z 
+	else if (x=="Divide") {
+		x / z; 
 	}
 	else {
-		output = "assign a operator"
+		output = "set a operator";
 	}
-
-
-
-
-
-
 }
-
 var output = calculator(x,y,z);
 console.log(output);
